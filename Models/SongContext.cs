b9c64446a00262b720f8828c9e84cba1596ff830
luongdoan277 <luongdoan277@gmail.com>
2020-09-25 11:01:01 +0700

    lab2

---
+++ Models/SongContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebMusic.Models
+{
+    public class SongContext : DbContext
+    {
+        public SongContext() : base("Music")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
